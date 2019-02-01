de3a5470877db4c9a1a415d8870b46e7cb3aefe9
Eric <diliu1@Dis-MacBook-Pro.local>
2019-02-01 09:54:11 -0800

    1
    1

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const port=process.env.PORT || 3000
+app.get('/', (req, res) => res.send('Hello World!'))
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World from Eric</h1>');
-});
-
-server.listen(port,() => {
-    console.log(`Server running at port `+port);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
